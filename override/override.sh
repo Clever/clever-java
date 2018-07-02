ed857f48e0f369e377785f696de7b7efa3a0f678
Amelia Jones <amelia.jones@clever.com>
2018-07-02 16:34:35 -0700

    Add Linux sed command for Circle

--- override/override.sh
+++ override/override.sh
@@ -1,3 +1,9 @@
 cp override/EventsApiClassTest.java src/test/java/io/swagger/client/api/EventsApiClassTest.java
 
-sed -i "" 's/<name>client<\/name>/<name>Clever Java Client<\/name>/g' pom.xml
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    sed -i "" 's/<name>client<\/name>/<name>Clever Java Client<\/name>/g' pom.xml
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    sed -i"" 's/<name>client<\/name>/<name>Clever Java Client<\/name>/g' pom.xml
+fi
