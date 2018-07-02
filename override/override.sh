e47f2b39d09c6164c56e77f2047801656024fbc5
Amelia Jones <amelia.jones@clever.com>
2018-07-01 21:31:51 -0700

    Update overrides and add 'make generate'

--- override/override.sh
+++ override/override.sh
@@ -1,7 +1,3 @@
-# Note that these files are based off the files in:
-# https://github.com/swagger-api/swagger-codegen/tree/master/modules/swagger-codegen/src/main/resources/Java/libraries/okhttp-gson
+cp override/EventsApiClassTest.java src/test/java/io/swagger/client/api/EventsApiClassTest.java
 
-cp build.sbt ../build.sbt
-cp pom.xml ../pom.xml
-cp JSON.java ../src/main/java/io/swagger/client/JSON.java
-cp EventsApiClassTest.java ../src/test/java/io/swagger/client/api/EventsApiClassTest.java
+sed -i "" 's/<name>client<\/name>/<name>Clever Java Client<\/name>/g' pom.xml
